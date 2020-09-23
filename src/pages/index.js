f22e95a0ad065a9e46ef9f91d942befc7c4b4c92
Dmitrij Kiltau <13748577+dmitrijkiltau@users.noreply.github.com>
2020-09-23 21:04:21 +0200

    remove StaticQuery

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { graphql, StaticQuery } from "gatsby"
+import { graphql } from "gatsby"
 import { Helmet } from "react-helmet"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { library } from "@fortawesome/fontawesome-svg-core"
@@ -26,69 +26,65 @@ function MenuItem(props) {
   )
 }
 
-export default function Home() {
+export const query = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+        description
+      }
+    }
+  }
+`
+
+export default function Home({ data }) {
   return (
-    <StaticQuery
-      query={graphql`
-        query {
-          site {
-            siteMetadata {
-              title
-              description
-            }
-          }
-        }
-      `}
-      render={data => (
-        <div className="App">
-          <Helmet
-            title={data.site.siteMetadata.title}
-            description={data.site.siteMetadata.description}
-          />
+    <div className="App">
+      <Helmet
+        title={data.site.siteMetadata.title}
+        description={data.site.siteMetadata.description}
+      />
 
-          <header>
-            <div id="logo">
-              <Logo />
-            </div>
-          </header>
-
-          <menu>
-            {social.menu.map(item => (
-              <MenuItem key={item.name} item={item} />
-            ))}
-          </menu>
-
-          <main>
-            <div id="picture">
-              <img src={"/images/ich.jpg"} alt="Dima" />
-            </div>
-
-            <div id="content">
-              <h1>
-                Hi! Mein Name ist <s>Dmitrij Kiltau</s> Dima.
-              </h1>
-              <p>
-                Angefangen als Hobby Web- & App-Entwickler bin ich mittlerweile
-                in einer Ausbildung als Fachinformatiker für
-                Anwendungsentwicklung. Außerdem probiere ich mich etwas an UI &
-                UX Design oder drehe & schneide hin und wieder (Musik-)Videos
-                für Freunde.
-              </p>
-              <p>
-                Hauptberuflich bin ich eher im Bereich Web-Entwicklung (HTML,
-                CSS, JS, PHP & MySQL) unterwegs. Privat wiederum im Bereich der
-                App-Entwicklung (Kotlin, Flutter/Dart).
-              </p>
-            </div>
-          </main>
-
-          <footer>
-            {social.footer.map(item => (
-              <MenuItem key={item.name} item={item} />
-            ))}
-          </footer>
+      <header>
+        <div id="logo">
+          <Logo />
         </div>
-      )}
-    />
+      </header>
+
+      <menu>
+        {social.menu.map(item => (
+          <MenuItem key={item.name} item={item} />
+        ))}
+      </menu>
+
+      <main>
+        <div id="picture">
+          <img src={"/images/ich.jpg"} alt="Dima" />
+        </div>
+
+        <div id="content">
+          <h1>
+            Hi! Mein Name ist <s>Dmitrij Kiltau</s> Dima.
+          </h1>
+          <p>
+            Angefangen als Hobby Web- & App-Entwickler bin ich mittlerweile in
+            einer Ausbildung als Fachinformatiker für Anwendungsentwicklung.
+            Außerdem probiere ich mich etwas an UI & UX Design oder drehe &
+            schneide hin und wieder (Musik-)Videos für Freunde.
+          </p>
+          <p>
+            Hauptberuflich bin ich eher im Bereich Web-Entwicklung (HTML, CSS,
+            JS, PHP & MySQL) unterwegs. Privat wiederum im Bereich der
+            App-Entwicklung (Kotlin, Flutter/Dart).
+          </p>
+        </div>
+      </main>
+
+      <footer>
+        {social.footer.map(item => (
+          <MenuItem key={item.name} item={item} />
+        ))}
+      </footer>
+    </div>
   )
 }
