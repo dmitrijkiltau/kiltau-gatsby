ae64703951df2c373bff336d51092cb0d617e003
Dmitrij Kiltau <13748577+dmitrijkiltau@users.noreply.github.com>
2020-11-29 16:35:04 +0100

    add more animations

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import { Helmet } from "react-helmet"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { library } from "@fortawesome/fontawesome-svg-core"
 import { fab } from "@fortawesome/free-brands-svg-icons"
-import { gsap, TimelineLite, CSSPlugin, Power1 } from "gsap/all"
+import { gsap, TimelineLite, CSSPlugin, Power4 } from "gsap/all"
 import Logo from "../assets/logo.svg"
 import social from "../data/social.json"
 import "../scss/styles.scss"
@@ -12,7 +12,7 @@ import "../scss/styles.scss"
 library.add(fab)
 gsap.registerPlugin(CSSPlugin)
 
-function MenuItem(props) {
+const MenuItem = props => {
   return (
     <a
       href={props.item.href}
@@ -35,22 +35,37 @@ const Home = () => {
   const pictureRef = createRef()
   const contentRef = createRef()
 
+  const menuItemAnimation =
+    window.outerWidth > 420
+      ? { x: -150, ease: Power4.easeOut }
+      : { y: -150, ease: Power4.easeOut }
+
   useEffect(() => {
     timeline
+      // Logo animation.
+      .fromTo(
+        "#logo svg",
+        { opacity: 0, scale: 0, rotate: -180 },
+        { opacity: 1, scale: 1, rotate: 0, ease: Power4.easeOut, duration: 2 }
+      )
+      // Menu items animation.
+      .staggerFrom(".menu-item", 1, menuItemAnimation, 0.2)
       // Picture animation.
       .fromTo(
         pictureRef.current,
-        { opacity: 0, y: -100 },
-        { opacity: 1, y: 0, ease: Power1.easeOut, duration: 2 },
+        { opacity: 0, y: -1000, delay: 1 },
+        { opacity: 1, y: 0, ease: Power4.easeOut, duration: 3 },
         0
       )
       // Content animation.
       .fromTo(
         contentRef.current,
-        { opacity: 0, y: 200 },
-        { opacity: 1, y: 0, ease: Power1.easeOut, duration: 2 },
+        { opacity: 0, y: 2000, delay: 1 },
+        { opacity: 1, y: 0, ease: Power4.easeOut, duration: 3 },
         0
       )
+      // Hide scrollbar while animation is running.
+      .fromTo(appRef.current, { overflow: "hidden" }, { overflow: "auto" })
       .play()
   })
 
